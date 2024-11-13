b4490eed8d651381cb7164d45554f9c45bb7ec3a
Thiago Terra <thiagoterradev@gmail.com>
2024-11-13 08:41:22 -0300

    feat: adds react router dom

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,11 @@
-export { };
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/home" element={<p>Home page</p>} />
+
+      <Route path="*" element={<Navigate to="/home" />} />
+    </Routes>
+  )
+};
