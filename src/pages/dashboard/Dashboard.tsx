e4474f6fa8a26fdef4af66f34596cebbec98a5e7
Thiago Terra <thiagoterradev@gmail.com>
2024-11-29 18:54:18 -0300

    feat: implements base page layout and dashboard components

---
+++ src/pages/dashboard/Dashboard.tsx
@@ -0,0 +1,10 @@
+import { BasePageLayout } from "../../shared/layouts";
+
+
+export const Dashboard = () => {
+  return (
+    <BasePageLayout title="Home" toolbar={<>Barra de ferramentas</>}>
+      Testando
+    </BasePageLayout>
+  );
+};
---
+++ src/shared/layouts/BasePageLayout.tsx
@@ -0,0 +1,52 @@
+import { Box, Icon, IconButton, Typography, useMediaQuery, useTheme } from "@mui/material";
+import { useDrawerContext } from "../contexts";
+
+
+interface IBasePageLayoutProps {
+  children: React.ReactNode;
+  title: string;
+  toolbar?: React.ReactNode;
+}
+
+export const BasePageLayout: React.FC<IBasePageLayoutProps> = ({ children, title, toolbar }) => {
+  const theme = useTheme();
+  const smDown = useMediaQuery(theme.breakpoints.down("sm"));
+  const mdDown = useMediaQuery(theme.breakpoints.down("md"));
+
+  const { toggleDrawerOpen } = useDrawerContext();
+
+  return (
+    <Box height="100%" display="flex" flexDirection="column" gap="1">
+      <Box 
+        padding={1} 
+        height={theme.spacing(smDown ? 6 : mdDown ? 8 : 12)} 
+        display="flex" 
+        alignItems="center" gap="1">
+
+        {smDown &&
+          <IconButton onClick={toggleDrawerOpen}>
+            <Icon>menu</Icon>
+          </IconButton>
+        }
+
+        <Typography 
+          variant={smDown ? "h5" : mdDown ? "h4" : "h3"} 
+          whiteSpace="nowrap" 
+          overflow="hidden" 
+          textOverflow="ellipses">
+          {title}
+        </Typography>
+      </Box>
+
+      {toolbar &&
+        <Typography>
+          {toolbar}
+        </Typography>
+      }
+
+      <Box flex={1} overflow="auto">
+        {children}
+      </Box>
+    </Box>
+  );
+};
