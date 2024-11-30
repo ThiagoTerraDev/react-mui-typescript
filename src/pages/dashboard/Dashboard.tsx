d12986fe50a5202085b78f2f9b4fc6bfeea075c9
Thiago Terra <thiagoterradev@gmail.com>
2024-11-30 16:32:07 -0300

    feat: implements listing toolbar component

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,9 +1,17 @@
+import { Toolbar } from "../../shared/components";
 import { BasePageLayout } from "../../shared/layouts";
 
 
 export const Dashboard = () => {
   return (
-    <BasePageLayout title="Home" toolbar={<>Toolbar</>}>
+    <BasePageLayout 
+      title="Home" 
+      toolbar={(
+        <Toolbar 
+          showTextField
+        />
+      )}
+    >
       Testing
     </BasePageLayout>
   );
---
+++ src/shared/components/toolbar/Toolbar.tsx
@@ -0,0 +1,59 @@
+import { Box, Button, Icon, Paper, TextField, useTheme } from "@mui/material";
+
+
+interface IToolbarProps {
+  searchText?: string;
+  showTextField?: boolean;
+  onChangeSearchText?: (newSearchText: string) => void;
+  newButtonText?: string;
+  showNewButton?: boolean;
+  onClickNewButton?: () => void;
+}
+
+export const Toolbar: React.FC<IToolbarProps> = ({ 
+  searchText = "", 
+  showTextField = false, 
+  onChangeSearchText,
+  newButtonText = "New",
+  showNewButton = true,
+  onClickNewButton
+}) => {
+
+  const theme = useTheme();
+
+  return (
+    <Box 
+      component={Paper}
+      height={theme.spacing(5)} 
+      marginX={1}
+      padding={1}
+      paddingX={2}
+      display="flex"
+      alignItems="center"
+      gap={1}
+    >
+      {showTextField && 
+        <TextField 
+          size="small" 
+          placeholder="Search..."
+          value={searchText}
+          onChange={(e) => onChangeSearchText?.(e.target.value)} 
+        />
+      }
+
+      <Box flex={1} display="flex" justifyContent="end">
+        {showNewButton &&
+          <Button 
+            variant="contained" 
+            color="primary" 
+            disableElevation
+            endIcon={<Icon>add</Icon>}
+            onClick={onClickNewButton}
+          >
+            {newButtonText}
+          </Button>
+        }
+      </Box>
+    </Box>
+  );
+};
