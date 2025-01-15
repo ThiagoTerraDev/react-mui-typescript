a9470cee80968f9b2dc7df74d87ef6b3f8398973
Thiago Terra <thiagoterradev@gmail.com>
2025-01-15 16:50:32 -0300

    feat: implement form usage with data validation

--- src/pages/members/MembersDetails.tsx
+++ src/pages/members/MembersDetails.tsx
@@ -3,8 +3,19 @@ import { BasePageLayout } from "../../shared/layouts";
 import { DetailsToolbar } from "../../shared/components";
 import { useEffect, useState } from "react";
 import { MembersService } from "../../shared/services/api/members/MembersService";
-import { LinearProgress } from "@mui/material";
+import { Box, Grid2, LinearProgress, Paper, TextField, Typography } from "@mui/material";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
+
+const memberValidationSchema = z.object({
+  fullName: z.string().nonempty("Fullname is required"),
+  email: z.string().email("Please enter a valid email"),
+  cityId: z.preprocess((value) => Number(value), z.number().min(1, "CityId must be greater than 0")),
+});
+
+type MemberValidationSchema = z.infer<typeof memberValidationSchema>;
 
 export const MembersDetails: React.FC = () => {
   const { id = "new" } = useParams<"id">();
@@ -12,6 +23,15 @@ export const MembersDetails: React.FC = () => {
 
   const [isLoading, setIsLoading] = useState(false);
   const [memberFullName, setMemberFullName] = useState("");
+
+  const { register, handleSubmit, reset, formState: { errors } } = useForm<MemberValidationSchema>({
+    resolver: zodResolver(memberValidationSchema),
+    defaultValues: {
+      fullName: "",
+      email: "",
+      cityId: undefined,
+    }
+  });
 
   useEffect(() => {
     if (id !== "new") {
@@ -25,11 +45,55 @@ export const MembersDetails: React.FC = () => {
             navigate("/members");
           } else {
             setMemberFullName(result.fullName);
-            console.log(result);
+            reset(result);
+          }
+        });
+    } else {
+      reset({
+        fullName: "",
+        email: "",
+        cityId: 0,
+      });
+    }
+  }, [id]);
+
+  const handleSave = (data: MemberValidationSchema, goBack: boolean) => {
+    setIsLoading(true);
+
+    if (id === "new") {
+      MembersService.create(data)
+        .then((result) => {
+          setIsLoading(false);
+
+          if (result instanceof Error) {
+            alert(result.message);
+          } else {
+            alert("Member created successfully");
+            if (goBack) {
+              navigate("/members");
+            } else {
+              navigate(`/members/details/${result}`);
+            }
+          }
+        });
+    } else {
+      MembersService.updateById(Number(id), { id: Number(id), ...data })
+        .then((result) => {
+          setIsLoading(false);
+
+          if (result instanceof Error) {
+            alert(result.message);
+          } else {
+            alert("Member updated successfully");
+            if (goBack) {
+              navigate("/members");
+            } else {
+              setMemberFullName(data.fullName);
+            }
           }
         });
     }
-  }, []);
+  };
 
   const handleDelete = (id: number) => {
     if (window.confirm("Are you sure you want to delete this member?")) {
@@ -55,17 +119,95 @@ export const MembersDetails: React.FC = () => {
           showNewButton={id !== "new"}
           onClickBackButton={() => navigate("/members")}
           onClickNewButton={() => navigate("/members/details/new")}
-          // onClickSaveButton={handleSave}
+          onClickSaveButton={handleSubmit((data) => handleSave(data, false))}
           onClickDeleteButton={() => handleDelete(Number(id))}
-          // onClickSaveAndBackButton={handleSaveAndBack}
+          onClickSaveAndBackButton={handleSubmit((data) => handleSave(data, true))}
         />
       }
     >
-      {isLoading && (
-        <LinearProgress variant="indeterminate"/>
-      )}
 
-      Details
+      <form>
+        <Box 
+          margin={1} 
+          display="flex" 
+          flexDirection="column" 
+          component={Paper}
+          variant="outlined"
+        >
+          <Grid2 container direction="column" padding={2} spacing={2}>
+            
+            {isLoading && (
+              <Grid2>
+                <LinearProgress variant="indeterminate"/>
+              </Grid2>
+            )}
+            
+            <Grid2 mb={1}>
+              <Typography variant="h6">
+                Member Details
+              </Typography>
+            </Grid2>
+
+            <Grid2 container direction="row">
+              <Grid2 size={{ xs: 12, md: 6, lg: 4, xl: 3 }}>
+                <TextField
+                  fullWidth 
+                  slotProps={{
+                    inputLabel: { shrink: true },
+                    formHelperText: {
+                      sx: {
+                        maxHeight: "20px",
+                      },
+                    }}}
+                  label="Fullname" 
+                  {...register("fullName")} 
+                  disabled={isLoading}
+                  error={!!errors.fullName}
+                  helperText={errors.fullName?.message}
+                />
+              </Grid2>
+            </Grid2>
+            <Grid2 container direction="row">
+              <Grid2 size={{ xs: 12, md: 6, lg: 4, xl: 3 }}>
+                <TextField
+                  fullWidth  
+                  slotProps={{
+                    inputLabel: { shrink: true },
+                    formHelperText: {
+                      sx: {
+                        maxHeight: "20px",
+                      },
+                    }}}
+                  label="Email" 
+                  {...register("email")}
+                  disabled={isLoading}
+                  error={!!errors.email}
+                  helperText={errors.email?.message}
+                />
+              </Grid2>
+            </Grid2>
+            <Grid2 container direction="row">
+              <Grid2 size={{ xs: 12, md: 6, lg: 4, xl: 3 }}>
+                <TextField
+                  fullWidth 
+                  slotProps={{
+                    inputLabel: { shrink: true },
+                    formHelperText: {
+                      sx: {
+                        maxHeight: "20px",
+                      },
+                    }}}
+                  label="CityId" 
+                  {...register("cityId")}
+                  disabled={isLoading}
+                  error={!!errors.cityId}
+                  helperText={errors.cityId?.message}
+                />
+              </Grid2>
+            </Grid2>
+          </Grid2>
+        </Box>
+      </form>
     </BasePageLayout>
   );
 };
